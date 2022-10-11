95a9ea86aaf1ad92d41c777e67e715f82251ca35
DanielBressan93 <danielbressan18@gmail.com>
2022-10-11 16:11:59 -0300

    realizado ajustes finais

--- assets/js/menu-mobile.js
+++ assets/js/menu-mobile.js
@@ -1,11 +1,17 @@
 // Menu Mobile
+const nav = document.querySelector('.navbar');
 const btnMenu = document.querySelector('.btn-menu');
+const menu = document.querySelector('.menu');
+const links = document.querySelectorAll('.link');
 const eventos = ['click', 'touchstart'];
 
 export default function toggleMenu(event) {
   if (event.type === 'touchstart') event.preventDefault();
-  const nav = document.querySelector('.navbar');
+  event.stopPropagation();
   nav.classList.toggle('ativo');
+  handleClickOutside(menu, () => {
+    nav.classList.remove('ativo');
+  });
   const ativo = nav.classList.contains('ativo');
   event.currentTarget.setAttribute('aria-expanded', ativo);
   if (ativo) {
@@ -15,6 +21,29 @@ export default function toggleMenu(event) {
   }
 }
 
+function handleClickOutside(targetElement, callback) {
+  const html = document.documentElement;
+  function handleHTMLClick(event) {
+    if (!targetElement.contains(event.target)) {
+      targetElement.removeAttribute('data-target');
+      html.removeEventListener('click', handleHTMLClick);
+      html.removeEventListener('touchstart', handleHTMLClick);
+      callback();
+    }
+  }
+  if (!targetElement.hasAttribute('data-target')) {
+    html.addEventListener('click', handleHTMLClick);
+    html.addEventListener('touchstart', handleHTMLClick);
+    targetElement.setAttribute('data-target', '');
+  }
+}
+
 eventos.forEach((evento) => {
   btnMenu.addEventListener(evento, toggleMenu);
+});
+
+links.forEach((link) => {
+  link.addEventListener('touchstart', () => {
+    nav.classList.remove('ativo');
+  });
 });
